281ad555f8e4315a46506b25e200fbc94a3b39c0
UDONKONET <udonkonet@UDONKONETnoMacBook.local>
2017-05-14 17:40:32 +0900

    ハッカソン用プロジェクト作成

---
+++ HackathonProject/HackathonProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackathonProject
+//
+//  Created by UDONKONET on 2017/05/14.
+//  Copyright © 2017年 UDONKONET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HackathonProject/HackathonProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackathonProject
+//
+//  Created by UDONKONET on 2017/05/14.
+//  Copyright © 2017年 UDONKONET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
